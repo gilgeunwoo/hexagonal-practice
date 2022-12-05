b6761f4199162439f4dd332819b240490e1325da
gilgeunwoo <kesksjxhx@gmail.com>
2022-12-05 23:34:44 +0900

    transaction aop 세팅

---
+++ hexagonalpractice-infrastructure/src/main/java/com/example/hexagonalpractice/global/aop/TransactionAspect.java
@@ -0,0 +1,69 @@
+package com.example.hexagonalpractice.global.aop;
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.interceptor.MatchAlwaysTransactionAttributeSource;
+import org.springframework.transaction.interceptor.RollbackRuleAttribute;
+import org.springframework.transaction.interceptor.RuleBasedTransactionAttribute;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
+
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Configuration
+@Aspect
+public class TransactionAspect {
+
+    private final TransactionManager transactionManager;
+
+    @Bean
+    public Advisor transactionAdviceAdvisor() {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("@within(com.example.hexagonalpractice.annotation.UseCase)");
+
+        return new DefaultPointcutAdvisor(pointcut, transactionAdvice());
+    }
+
+    @Bean
+    public Advisor readOnlyTransactionAdviceAdvisor() {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("@within(com.example.hexagonalpractice.annotation.ReadOnlyUseCase)");
+
+        return new  DefaultPointcutAdvisor(pointcut, readOnlyTransactionAdvice());
+    }
+
+    @Bean
+    public TransactionInterceptor transactionAdvice() {
+        MatchAlwaysTransactionAttributeSource source = new MatchAlwaysTransactionAttributeSource();
+        RuleBasedTransactionAttribute transactionAttribute = new RuleBasedTransactionAttribute();
+
+        transactionAttribute.setName("Transaction");
+        transactionAttribute.setRollbackRules(
+                Collections.singletonList(new RollbackRuleAttribute(Exception.class))
+        );
+        source.setTransactionAttribute(transactionAttribute);
+
+        return new TransactionInterceptor(transactionManager, source);
+    }
+
+    @Bean
+    public TransactionInterceptor readOnlyTransactionAdvice() {
+        MatchAlwaysTransactionAttributeSource source = new MatchAlwaysTransactionAttributeSource();
+        RuleBasedTransactionAttribute transactionAttribute = new RuleBasedTransactionAttribute();
+
+        transactionAttribute.setName("Read-Only Transaction");
+        transactionAttribute.setReadOnly(true);
+        transactionAttribute.setRollbackRules(
+                Collections.singletonList(new RollbackRuleAttribute(Exception.class))
+        );
+        source.setTransactionAttribute(transactionAttribute);
+
+        return new TransactionInterceptor(transactionManager, source);
+    }
+}
