0d223af144a52674546781ff61bbe684cb81e72d
gilgeunwoo <kesksjxhx@gmail.com>
2022-12-08 16:15:10 +0900

    CommendFeedPort 추가

---
+++ hexagonalpractice-application/src/main/java/com/example/hexagonalpractice/domain/feed/spi/CommendFeedPort.java
@@ -0,0 +1,11 @@
+package com.example.hexagonalpractice.domain.feed.spi;
+
+import com.example.hexagonalpractice.domain.feed.domain.Feed;
+
+import java.util.UUID;
+
+public interface CommendFeedPort {
+
+    void saveFeed(Feed feed);
+    UUID saveFeedAndGetId(Feed feed);
+}
